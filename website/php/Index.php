d393ecdf01c33fe7001320394e74aafb925aaf5a
74542634 <ps221768@edu.summacollege.nl>
2021-06-16 11:01:14 +0200

    (hard code naar include verandert (index.php))

--- website/php/Index.php
+++ website/php/Index.php
@@ -6,25 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Homepagina</title>
     <link rel="stylesheet" href="css/Verkiezingen.css" />
-    <link rel="stylesheet" href="css/navigatiebalk.css" />
-</head>
-<body>
-        <div class="nav-bar">
-            <ul>
-                <li><a href="navigatiebalk.html">Pagina naam</a></li>
-                <li><a href="navigatiebalk.html">Pagina naam</a></li>
-                <li><a href="navigatiebalk.html">Pagina naam</a></li>
-            </ul>
-        </div>
 
-      <h2>Doel van de website en de verkiezingen:</h2>
-      <div class="Home">
-        <p>
-            Wij willen de kiezers stimuleren om naar de stembus te gaan met een goede en slimme doordachte keuze. 
-            <br>Als standpunten van de politieke partijen overzichtelijk en duidelijk zijn, 
-            is men beter in staat om op een politieke partij te stemmen die het beste bij ze past.
-            <br> Dus hopen wij dat wij u kunnen helpen met de jeuste stemkeuze. 
-        </p>
-    </div>
+</head>
+<?php
+include 'navigatiebalk.php';
+?>
+<body>
+    <h2>Doel van de website en de verkiezingen:</h2>
+    <div class="Home">
+    <p>
+        Wij willen de kiezers stimuleren om naar de stembus te gaan met een goede en slimme doordachte keuze. 
+        <br>Als standpunten van de politieke partijen overzichtelijk en duidelijk zijn, 
+        is men beter in staat om op een politieke partij te stemmen die het beste bij ze past.
+        <br> Dus hopen wij dat wij u kunnen helpen met de jeuste stemkeuze. 
+    </p>
 </body>
 </html>
